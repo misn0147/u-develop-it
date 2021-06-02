6c3f893cc873cf1f58c2ce19129b16e2afbbd62f
M Natoli <natoli.missy@gmail.com>
2021-06-02 18:10:09 -0500

    get votes method

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: 'TCisland20',
-  database: 'election'
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'TCisland20',
+    database: 'election'
 });
 
 module.exports = db;
